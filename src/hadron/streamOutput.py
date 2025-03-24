2c90a344125573813e3f245cdb711352976d50cf
layanka <ici.snoopy@gmail.com>
2025-03-23 21:58:20 -0400

    Initial uploads of files

---
+++ src/hadron/streamOutput.py
@@ -0,0 +1,13 @@
+import io
+from threading import Condition
+
+# Class to handle streaming output
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
